9743d624e141abf914ece2d5c7a57c2b6437dfa7
jsivakumaran <jsivakumaran@outlook.com>
2018-11-06 20:19:40 -0500

    Fake the db using an array - temporary measure

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,6 +2,13 @@ const graphql = require('graphql');
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
+//faking the database data in an array for now
+var books = [
+  {name: 'Cujo', genre: 'Horror', id: '1'},
+  {name: 'Handmaid\'s Tale', genre: 'Period', id: '2'},
+  {name: 'Pride and Prejudice', genre: 'Romance', id: '3'}
+];
+
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: ()=> ({
