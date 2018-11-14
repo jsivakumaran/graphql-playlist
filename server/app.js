e8412ee5e5732ed666d9223ffeae7e5ab36bf170
jsivakumaran <jsivakumaran@outlook.com>
2018-11-13 21:32:42 -0500

    Add connection to mongodb

--- server/app.js
+++ server/app.js
@@ -2,12 +2,21 @@ const express = require('express');
 
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const dotenv = require('dotenv');
+dotenv.config();
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect(process.env.MLAB_URL);
+mongoose.connection.once('open', ()=>{
+  console.log('connected to db');
+});
 
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true
 }));
+
 
 app.listen(4000, ()=>{
   console.log(`now listening on port 4000`);
