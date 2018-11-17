eb974ae58b4fdbaabaecb046b004e1540445df10
jsivakumaran <jsivakumaran@outlook.com>
2018-11-17 11:44:23 -0500

    create a basic static html component for BookList and render in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+//components
+import BookList from './components/BookList';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Jay's Reading List</h1>
+        <BookList />
       </div>
     );
   }
