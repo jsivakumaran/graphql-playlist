336169a2624590c3c1e78be7acec8dab64e219ec
jsivakumaran <jsivakumaran@outlook.com>
2018-11-18 20:45:11 -0500

    install apollo, apollo-react in order to do front-end queries from react to the grpahql server in backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo';
+
 
 //components
 import BookList from './components/BookList';
 
+//apollo client setup
+const client = new ApolloClient({
+  uri: 'http://localhost:8081/graphql'
+});
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Jay's Reading List</h1>
-        <BookList />
-      </div>
+      <ApolloProivder client={client}>
+        <div className="App">
+          <h1>Jay's Reading List</h1>
+          <BookList />
+        </div>
+      </ApolloProivder>
     );
   }
 }
