244bbddc3fee03d930adb2b2b032734caf47d2e8
jsivakumaran <jsivakumaran@outlook.com>
2018-11-21 17:53:18 -0500

    create form to addBook - plus populate list of authors in form

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import {ApolloProvider} from 'react-apollo';
 
 //components
 import BookList from './components/BookList';
+import AddBook from './components/AddBook';
 
 //apollo client setup
 const client = new ApolloClient({
@@ -18,6 +19,7 @@ class App extends Component {
         <div className="App">
           <h1>Jay's Reading List</h1>
           <BookList />
+          <AddBook />
         </div>
       </ApolloProvider>
     );
